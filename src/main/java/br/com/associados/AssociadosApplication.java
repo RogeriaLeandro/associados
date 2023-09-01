75668c6dc373a3a9a7d3b085967710c0effac09f
Rogéria Leandro <72103490+RogeriaLeandro@users.noreply.github.com>
2023-09-01 20:33:45 -0300

    Criação de TestConfig para inserção de dados no Banco (#15)
    Co-authored-by: RogeriaLeandro <RogeriaLeandro>

--- src/main/java/br/com/associados/AssociadosApplication.java
+++ src/main/java/br/com/associados/AssociadosApplication.java
@@ -1,13 +1,16 @@
 package br.com.associados;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.associados.model.Associado;
+import br.com.associados.repositories.AssociadoRepository;
+
 @SpringBootApplication
-public class AssociadosApplication {
+public class AssociadosApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssociadosApplication.class, args);
 	}
-
 }
---
+++ src/main/java/br/com/associados/config/TestConfig.java
@@ -0,0 +1,25 @@
+package br.com.associados.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.associados.model.Associado;
+import br.com.associados.repositories.AssociadoRepository;
+
+@Configuration
+public class TestConfig implements CommandLineRunner{
+    @Autowired
+    private AssociadoRepository associadoRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Associado associado1 = new Associado(null, "36104399126", "PF", "Associado 1");
+        Associado associado2 = new Associado(null, "71871128000179", "PJ", "Associado 2");
+
+        associadoRepository.save(associado1);
+        associadoRepository.save(associado2);
+    }
+}
+
+
