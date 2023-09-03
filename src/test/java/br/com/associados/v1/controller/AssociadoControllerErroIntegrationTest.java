23f88a6b447935a03786bae010bdc79edf0854da
Rogéria Leandro <72103490+RogeriaLeandro@users.noreply.github.com>
2023-09-03 19:26:02 -0300

    cobertura de testes 1 (#41)
    Co-authored-by: RogeriaLeandro <RogeriaLeandro>

---
+++ src/test/java/br/com/associados/v1/controller/AssociadoControllerErroIntegrationTest.java
@@ -0,0 +1,180 @@
+package br.com.associados.v1.controller;
+
+import br.com.associados.integracao.boleto.service.BoletoService;
+import br.com.associados.model.TipoPessoa;
+import br.com.associados.repositories.AssociadoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import br.com.associados.v1.dto.AssociadoRequestDTO;
+
+import java.util.UUID;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class AssociadoControllerErroIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AssociadoRepository associadoRepository;
+
+    @SpyBean
+    private BoletoService boletoService;
+
+    private static final String ENDPOINT_V1_ASSOCIADOS = "/v1/associados";
+    private static final String ENDPOINT_V1_ASSOCIADOS_ID = "/v1/associados/{id}";
+    private static final String ENDPOINT_V1_CONSULTAR_ASSOCIADO_POR_DOCUMENTO = "/v1/associados/documento";
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "  ", "abcd"})
+    @SneakyThrows
+    void identificadorAssociadoInvalidoAoConsultarAssociado(String idAssociado) {
+        mockMvc.perform(get(ENDPOINT_V1_ASSOCIADOS_ID, idAssociado))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("consultarAssociado.id: O ID deve ter formato de UUID"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "  ", "abcd"})
+    @SneakyThrows
+    void documentoAssociadoInvalidoAoConsultarAssociadoPorDocumento(String documento) {
+        mockMvc.perform(get(ENDPOINT_V1_CONSULTAR_ASSOCIADO_POR_DOCUMENTO)
+                        .queryParam("documento", documento))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("consultarAssociadoPorDocumento.documento: O documento deve ser um CPF ou CNPJ válido"));
+    }
+
+    @Test
+    @SneakyThrows
+    void falhaDocumentoInvalidoAoCadastrarAssociado() {
+        var associadoRequestDTO = AssociadoRequestDTO.builder()
+                .nome("joao")
+                .documento("1")
+                .tipoPessoa(TipoPessoa.PF)
+                .build();
+
+        var ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        var json = ow.writeValueAsString(associadoRequestDTO);
+
+        mockMvc.perform(post(ENDPOINT_V1_ASSOCIADOS)
+                        .content(json)
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void falhaNomeNaoInformadoAoCadastrarAssociado() {
+        var associadoRequestDTO = AssociadoRequestDTO.builder()
+                .documento("12312312387")
+                .tipoPessoa(TipoPessoa.PF)
+                .build();
+
+        var ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        var json = ow.writeValueAsString(associadoRequestDTO);
+
+        mockMvc.perform(post(ENDPOINT_V1_ASSOCIADOS)
+                        .content(json)
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @SneakyThrows
+    void falhaTipoPessoaInconsistenteAoCadastrarAssociado() {
+        var associadoRequestDTO = AssociadoRequestDTO.builder()
+                .nome("joao")
+                .documento("12312312312")
+                .tipoPessoa(TipoPessoa.PJ)
+                .build();
+
+        var ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        var json = ow.writeValueAsString(associadoRequestDTO);
+
+        mockMvc.perform(post(ENDPOINT_V1_ASSOCIADOS)
+                        .content(json)
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Tipo de documento não bate com o tipo pessoa informado"));
+    }
+
+    @Test
+    @SneakyThrows
+    void falhaTipoPessoaInconsistenteAoAlterarAssociado() {
+        var associado = associadoRepository.findAll(PageRequest.of(0, 1)).getContent().get(0);
+        var associadoRequestDTO = AssociadoRequestDTO.builder()
+                .nome("joao")
+                .documento("12312312312")
+                .tipoPessoa(TipoPessoa.PJ)
+                .build();
+
+        var ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        var json = ow.writeValueAsString(associadoRequestDTO);
+
+        mockMvc.perform(put(ENDPOINT_V1_ASSOCIADOS_ID, associado.getUuid())
+                        .content(json)
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Tipo de documento não bate com o tipo pessoa informado"));
+    }
+
+    @Test
+    @SneakyThrows
+    void falhaAssociadoNaoEncontradoAoAlterarAssociado() {
+        var associadoRequestDTO = AssociadoRequestDTO.builder()
+                .nome("joao")
+                .documento("12312312312")
+                .tipoPessoa(TipoPessoa.PJ)
+                .build();
+
+        var ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        var json = ow.writeValueAsString(associadoRequestDTO);
+        var id = UUID.randomUUID().toString();
+        mockMvc.perform(put(ENDPOINT_V1_ASSOCIADOS_ID, id)
+                        .content(json)
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Não foi possível encontrar um associado para o ID: " + id));
+    }
+
+    @Test
+    @SneakyThrows
+    void falhaAssociadoNaoEncontradoAoDeletarAssociado() {
+        var id = UUID.randomUUID().toString();
+        mockMvc.perform(delete(ENDPOINT_V1_ASSOCIADOS_ID, id)
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Não foi possível encontrar um associado para o ID: " + id));
+    }
+
+    @Test
+    @SneakyThrows
+    void falhaAssociadoComBoletoEmAbertoAoDeletarAssociado() {
+        var associado = associadoRepository.findAll(PageRequest.of(0, 1)).getContent().get(0);
+        //TODO: desligar mock quando subir api de boletos
+        doReturn(true).when(boletoService).possuiBoletoAPagar(associado.getUuid().toString());
+        mockMvc.perform(delete(ENDPOINT_V1_ASSOCIADOS_ID, associado.getUuid())
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$").value("Não é possível deletar um associado com boleto em aberto"));
+    }
+
+}
---
+++ src/test/java/br/com/associados/v1/service/AssociadoServiceTest.java
@@ -0,0 +1,211 @@
+package br.com.associados.v1.service;
+
+import br.com.associados.exceptions.AssociadoNaoEncontradoException;
+import br.com.associados.exceptions.BoletoEmAbertoException;
+import br.com.associados.exceptions.FormatoDocumentoInvalidoException;
+import br.com.associados.exceptions.TipoPessoaInconsistenteException;
+import br.com.associados.integracao.boleto.service.BoletoService;
+import br.com.associados.model.Associado;
+import br.com.associados.repositories.AssociadoRepository;
+import br.com.associados.v1.services.AssociadoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static br.com.associados.AssociadoTestHelper.*;
+import static br.com.associados.model.TipoPessoa.PF;
+import static br.com.associados.model.TipoPessoa.PJ;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AssociadoServiceTest {
+
+    @Mock
+    private AssociadoRepository associadoRepository;
+
+    @Mock
+    private BoletoService boletoService;
+
+    @InjectMocks
+    private AssociadoService target;
+
+    @Test
+    void consultarAssociados() {
+        var associados = criarAssociados();
+        Page<Associado> pageAssociados = new PageImpl(associados);
+        var associadosDTO = criarAssociadosDTO(associados);
+        doReturn(pageAssociados).when(associadoRepository).findAll(PageRequest.of(1 - 1, 5));
+        var actual = target.consultarAssociado("1");
+        assertEquals(associadosDTO, actual);
+        verify(associadoRepository).findAll(PageRequest.of(1 - 1, 5));
+        verifyNoInteractions(boletoService);
+    }
+
+    @Test
+    void nenhumAssociadoEncontradoAoConsultarAssociados() {
+        Page<Associado> pageAssociados = new PageImpl(List.of());
+        doReturn(pageAssociados).when(associadoRepository).findAll(PageRequest.of(1 - 1, 5));
+        var actual = target.consultarAssociado("1");
+        assertEquals(List.of(), actual);
+        verify(associadoRepository).findAll(PageRequest.of(1 - 1, 5));
+        verifyNoInteractions(boletoService);
+    }
+
+    @Test
+    void consultarAssociado() {
+        var associado = criarAssociados().get(0);
+        associado.setUuid(UUID.fromString(ID_ASSOCIADO));
+        var associadoDTO = Optional.of(criarAssociadoDTO(associado));
+        doReturn(Optional.of(associado)).when(associadoRepository).findById(ID_ASSOCIADO);
+        var actual = target.consultarAssociado(ID_ASSOCIADO);
+        assertEquals(associadoDTO, actual);
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verifyNoInteractions(boletoService);
+    }
+
+    @Test
+    void nenhumAssociadoEncontradoAoConsultarAssociado() {
+        doReturn(Optional.empty()).when(associadoRepository).findById(ID_ASSOCIADO);
+        var actual = target.consultarAssociado(ID_ASSOCIADO);
+        assertEquals(Optional.empty(), actual);
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verifyNoInteractions(boletoService);
+    }
+
+    @Test
+    void consultarAssociadoPorDocumento() {
+        var associado = criarAssociados().get(0);
+        associado.setDocumento(CPF);
+        var associadoDTO = Optional.of(criarAssociadoDTO(associado));
+        doReturn(Optional.of(associado)).when(associadoRepository).findByDocumento(CPF_NAO_FORMATADO);
+        var actual = target.consultarAssociadoPorDocumento(CPF);
+        assertEquals(associadoDTO, actual);
+        verify(associadoRepository).findByDocumento(CPF_NAO_FORMATADO);
+        verifyNoInteractions(boletoService);
+    }
+
+    @Test
+    void nenhumAssociadoEncontradoAoConsultarAssociadoPorDocumento() {
+        doReturn(Optional.empty()).when(associadoRepository).findByDocumento(CPF_NAO_FORMATADO);
+        var actual = target.consultarAssociadoPorDocumento(CPF);
+        assertEquals(Optional.empty(), actual);
+        verify(associadoRepository).findByDocumento(CPF_NAO_FORMATADO);
+        verifyNoInteractions(boletoService);
+    }
+
+    @Test
+    void cadastrarAssociado() {
+        var associadoRequest = criarAssociadoRequest(PF);
+        var associado = criarAssociado(associadoRequest);
+        var associadoDTO = criarAssociadoDTO(associadoRequest);
+        doReturn(associado).when(associadoRepository).save(refEq(associado, "id"));
+        var actual = target.cadastrarAssociado(associadoRequest);
+        associadoDTO.setId(associado.getUuid().toString());
+        assertEquals(associadoDTO, actual);
+        verify(associadoRepository).save(refEq(associado, "id"));
+        verifyNoInteractions(boletoService);
+    }
+
+    @Test
+    void erroAssociadoNaoEncontradoAoAlterarAssociado() {
+        var associadoRequest = criarAssociadoRequest(PJ);
+        doReturn(Optional.empty()).when(associadoRepository).findById(ID_ASSOCIADO);
+        var exception = assertThrows(AssociadoNaoEncontradoException.class, () -> target.alterarAssociado(ID_ASSOCIADO, associadoRequest));
+        assertEquals("Não foi possível encontrar um associado para o ID: " + ID_ASSOCIADO, exception.getMessage());
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verifyNoInteractions(boletoService);
+        verifyNoMoreInteractions(associadoRepository);
+    }
+
+    @Test
+    void erroDocumentoInvalidoAoAlterarAssociado() {
+        var associadoRequest = criarAssociadoRequest(PF);
+        associadoRequest.setDocumento(CPF + "A");
+        var associado = criarAssociados().get(0);
+        doReturn(Optional.of(associado)).when(associadoRepository).findById(ID_ASSOCIADO);
+        var exception = assertThrows(FormatoDocumentoInvalidoException.class, () -> target.alterarAssociado(ID_ASSOCIADO, associadoRequest));
+        assertEquals("O documento informado não é válido", exception.getMessage());
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verifyNoInteractions(boletoService);
+        verifyNoMoreInteractions(associadoRepository);
+    }
+
+    @Test
+    void erroTipoPessoaInconsistenteAoAlterarAssociado() {
+        var associadoRequest = criarAssociadoRequest(PJ);
+        associadoRequest.setDocumento(CPF);
+        var associado = criarAssociados().get(0);
+        doReturn(Optional.of(associado)).when(associadoRepository).findById(ID_ASSOCIADO);
+        var exception = assertThrows(TipoPessoaInconsistenteException.class, () -> target.alterarAssociado(ID_ASSOCIADO, associadoRequest));
+        assertEquals("Tipo de documento não bate com o tipo pessoa informado", exception.getMessage());
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verifyNoInteractions(boletoService);
+        verifyNoMoreInteractions(associadoRepository);
+    }
+
+    @Test
+    void alterarAssociado() {
+        var associadoRequest = criarAssociadoRequest(PJ);
+        associadoRequest.setNome("Nome alterado");
+        var associado = criarAssociados().get(0);
+        var associadoAlterado = criarAssociado(associadoRequest);
+        associadoAlterado.setUuid(associado.getUuid());
+        var associadoDTO = criarAssociadoDTO(associadoAlterado);
+        doReturn(Optional.of(associado)).when(associadoRepository).findById(ID_ASSOCIADO);
+        doReturn(associadoAlterado).when(associadoRepository).save(associadoAlterado);
+
+        var actual = target.alterarAssociado(ID_ASSOCIADO, associadoRequest);
+
+        assertEquals(associadoDTO, actual);
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verify(associadoRepository).save(associadoAlterado);
+        verifyNoInteractions(boletoService);
+    }
+
+    @Test
+    void erroAssociadoNaoEncontradoAoDeletarAssociado() {
+        doReturn(Optional.empty()).when(associadoRepository).findById(ID_ASSOCIADO);
+        var exception = assertThrows(AssociadoNaoEncontradoException.class, () -> target.deletarAssociado(ID_ASSOCIADO));
+        assertEquals("Não foi possível encontrar um associado para o ID: " + ID_ASSOCIADO, exception.getMessage());
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verifyNoInteractions(boletoService);
+        verifyNoMoreInteractions(associadoRepository);
+    }
+
+    @Test
+    void erroBoletoEmAbertoAoDeletarAssociado() {
+        var associado = criarAssociados().get(0);
+        associado.setUuid(UUID.fromString(ID_ASSOCIADO));
+        doReturn(Optional.of(associado)).when(associadoRepository).findById(ID_ASSOCIADO);
+        doReturn(true).when(boletoService).possuiBoletoAPagar(ID_ASSOCIADO);
+        var exception = assertThrows(BoletoEmAbertoException.class, () -> target.deletarAssociado(ID_ASSOCIADO));
+        assertEquals("Não é possível deletar um associado com boleto em aberto", exception.getMessage());
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verify(boletoService).possuiBoletoAPagar(ID_ASSOCIADO);
+        verifyNoMoreInteractions(associadoRepository);
+    }
+
+    @Test
+    void deletarAssociado() {
+        var associado = criarAssociados().get(0);
+        associado.setUuid(UUID.fromString(ID_ASSOCIADO));
+        doReturn(Optional.of(associado)).when(associadoRepository).findById(ID_ASSOCIADO);
+        doReturn(false).when(boletoService).possuiBoletoAPagar(ID_ASSOCIADO);
+        target.deletarAssociado(ID_ASSOCIADO);
+        verify(associadoRepository).findById(ID_ASSOCIADO);
+        verify(boletoService).possuiBoletoAPagar(ID_ASSOCIADO);
+        verify(associadoRepository).delete(associado);
+    }
+
+}
